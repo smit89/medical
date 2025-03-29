696b75683b9d1ed93880876afd4ede73af608324
smit89 <77917707+smit89@users.noreply.github.com>
2025-03-29 20:18:07 +0100

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = pd.read_csv('C:/vscode/kem-chho/medical/medical_examination.csv')
+df = pd.read_csv('medical_examination.csv')
 
 # 2
 height_m = df['height'] / 100
